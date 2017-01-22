a2168d0bfdfce7c09ab9424b0166e3fc5b087ead
王义泽 <1370700110@qq.com>
2017-01-22 21:11:27 +0800

    提交

---
+++ 课时5_2 访问限制/源.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class A {
+private:
+	int i;
+	int *p;
+public:
+	A() { p = 0; cout << "A::A() this = " << this << endl; }
+	~A() { if (p) delete p; cout << "A::~A() this = " << this << " i = " << i << endl; }
+	void set(int i) { this->i = i; }
+	void f() { p = new int; }
+	void g(A* q) { cout << "A::g(), q->i = " << q->i << endl; }
+};
+
+int main() {
+	A* p = new A[10];
+	for (int i = 0; i < 10; i++)
+		p[i].set(i);
+
+	A b;
+	b.set(100);
+	p[0].g(&b);
+	delete[] p;
+	return 0;
+}
+
